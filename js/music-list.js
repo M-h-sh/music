003f0709c1853158802422d4af0653bc4a8fb9e7
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2022-08-02 09:16:36 +0200

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,59 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "MaGear (Official Audio) feat. Mr Brown prod. by Master Azart ( 256kbps cbr )",
+    artist: "Makhadzi  ",
+    img: "music-1",
+    src: "music-1"
+  },
+ 
+  {
+    name: "Chipi Ke Chipi ( 256kbps cbr )",
+    artist: "Mellow & Sleazy  ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Kancane ft. Chley Konke & Nkulee501 ( 256kbps cbr )",
+    artist: "Musa Keys ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "WenzanuKabzaOfficial Audio",
+    artist: "Kabza De Small  Myztro ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Umuzi Wethu (Official Audio) ft. Madumane ( 256kbps cbr )",
+    artist: "Mlindo The Vocalist ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Jikelele-MPOSA.CO.ZA",
+    artist: "Mthandazo_Gatya_ft_Mvzzle",
+    img: "h",
+    src: "Mthandazo_Gatya_ft_Mvzzle_-_Jikelele-MPOSA.CO.ZA"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+
+];
